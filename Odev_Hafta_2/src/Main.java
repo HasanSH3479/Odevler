7e0fe63e8e2fdb6646bf333435b584a5560c683d
HasanSH3479 <hasansavascihabes34@gmail.com>
2024-10-17 16:41:06 +0300

    1. ve 2. haftanın ödevleri

---
+++ Odev_Hafta_2/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+    //1.Kısım
+    int a=40, b=20;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    //2. Kısım
+    int yukseklik= 15;
+    int taban_uzunlugu= 10;
+    System.out.print("Ucgenin alani=");
+    System.out.println(yukseklik*taban_uzunlugu/2);
+    System.out.print("Ucgenin cevresi=");
+    System.out.println(3*taban_uzunlugu);
+    //3. Kısım
+    int r=6; //daire yarıcapı
+    double pi= 3.14;
+    double karesi= Math.pow(r,2);
+    System.out.print("Dairenin alanı=");
+    System.out.println(karesi*pi);
+    System.out.print("Dairenin cevresi=");
+    System.out.println(pi*r*2);
+
+
+        }
+    }
